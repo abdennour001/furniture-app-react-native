4d752001aea9d9640edbbf07b45d6de2d3fdd49f
abdennour001 <amokranabdennour@gmail.com>
2020-11-07 19:24:35 +0100

    first commit 🔥

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    Platform,
+    SafeAreaView,
+    StatusBar,
+    TextInput
+} from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>Hello world!</Text>
+            <TextInput
+                style={{
+                    height: 30,
+                    backgroundColor: "lightgrey"
+                }}
+                value="useless"
+            ></TextInput>
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    }
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+# furniture-app-react-native
